9433912efa53707c09dd705bebe1388823dbe0b5
rcp.entwicklung@gmail.com <rcp.entwicklung@gmail.com@a6d18dd1-c050-23d8-ddb3-4fc8853fe964>
2012-04-10 11:18:06 +0000

    Initial version

---
+++ trunk/de.baeckerit.samples.rcp.view.simple/src/de/baeckerit/samples/rcp/view/simple/VerySimpleView.java
@@ -0,0 +1,66 @@
+package de.baeckerit.samples.rcp.view.simple;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.views.IViewDescriptor;
+
+public class VerySimpleView extends ViewPart {
+
+    private TableViewer viewer;
+
+    public void setFocus() {
+        viewer.getControl().setFocus();
+    }
+
+    public void createPartControl(Composite parent) {
+        viewer = new TableViewer(parent);
+        viewer.setContentProvider(ArrayContentProvider.getInstance());
+        viewer.setLabelProvider(new ViewLabelProvider());
+        viewer.setSorter(new ViewDescriptorSorter());
+        viewer.setInput(PlatformUI.getWorkbench().getViewRegistry().getViews());
+    }
+
+    class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
+        private ImageRegistry images = new ImageRegistry();
+        public String getColumnText(Object obj, int index) {
+            return ((IViewDescriptor) obj).getLabel();
+        }
+
+        public Image getColumnImage(Object obj, int index) {
+            IViewDescriptor viewDescriptor = (IViewDescriptor) obj;
+            Image image = images.get(viewDescriptor.getId());
+            if (image == null) {
+                ImageDescriptor imageDescriptor = viewDescriptor.getImageDescriptor();
+                if (imageDescriptor != null) {
+                    image = (Image) imageDescriptor.createImage(false, Display.getCurrent());
+                    if (image != null) {
+                        images.put(viewDescriptor.getId(), image);
+                    }
+                }
+            }
+            return image;
+        }
+
+        public void dispose() {
+            images.dispose();
+            super.dispose();
+        }
+    }
+
+    class ViewDescriptorSorter extends ViewerSorter {
+        public int compare(Viewer viewer, Object e1, Object e2) {
+            return ((IViewDescriptor) e1).getLabel().compareTo(((IViewDescriptor) e2).getLabel());
+        }
+    }
+}
