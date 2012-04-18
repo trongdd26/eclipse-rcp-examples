172204d58fa48f5473a84d4408af3cbba22b1ec8
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-04-18 08:21:20 +0000

    Initial version

--- de.baeckerit.samples.rcp.view.multiple/src/de/baeckerit/samples/rcp/view/multiple/ViewCategories.java
+++ de.baeckerit.samples.rcp.view.multiple/src/de/baeckerit/samples/rcp/view/multiple/ViewCategories.java
@@ -3,27 +3,27 @@ package de.baeckerit.samples.rcp.view.multiple;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.views.IViewCategory;
 
+import de.baeckerit.jface.util.JFACE;
+import de.baeckerit.jface.util.TableLabelProviderWithGetters;
+import de.baeckerit.jface.util.ViewerSorterWithGetter;
 import de.baeckerit.rcp.ui.getter.ViewCategoryImageGetter;
 import de.baeckerit.rcp.ui.getter.ViewCategoryLabelGetter;
 import de.baeckerit.rcp.ui.util.UI;
 import de.baeckerit.rcp.ui.view.ViewPartWithViewer;
-import de.baeckerit.rcp.ui.viewer.TableLabelProviderWithGetters;
-import de.baeckerit.rcp.ui.viewer.ViewerSorterWithGetter;
 
 public class ViewCategories extends ViewPartWithViewer<TableViewer> {
 
     public void createPartControl(Composite parent) {
         viewer = new TableViewer(parent);
         viewer.setContentProvider(ArrayContentProvider.getInstance());
-        ViewCategoryLabelGetter labelGetter = new ViewCategoryLabelGetter();
-        ViewCategoryImageGetter imageGetter = new ViewCategoryImageGetter();
+        ViewCategoryLabelGetter labelGetter = ViewCategoryLabelGetter.INSTANCE;
+        ViewCategoryImageGetter imageGetter = ViewCategoryImageGetter.INSTANCE;
         viewer.setLabelProvider(new TableLabelProviderWithGetters<>(labelGetter, imageGetter));
         viewer.setSorter(new ViewerSorterWithGetter<>(labelGetter));
         viewer.setInput(UI.getViewRegistry().getCategories());
@@ -32,8 +32,7 @@ public class ViewCategories extends ViewPartWithViewer<TableViewer> {
 
             @Override
             public void doubleClick(DoubleClickEvent event) {
-                IStructuredSelection selection = (IStructuredSelection) event.getSelection();
-                IViewCategory selected = (IViewCategory) selection.getFirstElement();
+                IViewCategory selected = JFACE.getFirstElement(IViewCategory.class, event);
                 try {
                     UI.getActivePage().showView(ViewDescriptorsByCategory.ID, selected.getId(),
                                     IWorkbenchPage.VIEW_ACTIVATE);
