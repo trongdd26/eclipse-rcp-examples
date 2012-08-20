0f8cf29243227e0f0adc43f9dcb470fca7e8b979
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-08-20 12:49:38 +0000

    Changes for PM example

--- de.baeckerit.rcp.ui.util/src/de/baeckerit/rcp/ui/util/EasyContentProvider.java
+++ de.baeckerit.rcp.ui.util/src/de/baeckerit/rcp/ui/util/EasyContentProvider.java
@@ -3,7 +3,7 @@ package de.baeckerit.rcp.ui.util;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
-import de.baeckerit.jface.util.JFACE;
+import de.baeckerit.jface.util.JFaceUtils;
 
 
 public class EasyContentProvider implements IStructuredContentProvider {
@@ -18,6 +18,6 @@ public class EasyContentProvider implements IStructuredContentProvider {
 
   @Override
   public Object[] getElements(Object inputElement) {
-    return JFACE.getElements(inputElement);
+    return JFaceUtils.getElements(inputElement);
   }
 }
