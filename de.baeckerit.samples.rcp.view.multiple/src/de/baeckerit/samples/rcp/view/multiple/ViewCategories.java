cbda7b9f863073c42d114e7be575af801bc3e746
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-08-20 09:55:34 +0000

    Changes for PM example

--- de.baeckerit.samples.rcp.view.multiple/src/de/baeckerit/samples/rcp/view/multiple/ViewCategories.java
+++ de.baeckerit.samples.rcp.view.multiple/src/de/baeckerit/samples/rcp/view/multiple/ViewCategories.java
@@ -10,8 +10,8 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.views.IViewCategory;
 
 import de.baeckerit.jface.util.JFACE;
-import de.baeckerit.jface.util.TableLabelProviderWithGetters;
-import de.baeckerit.jface.util.ViewerSorterWithGetter;
+import de.baeckerit.jface.util.SingleColumnTableLabelProviderWithGetters;
+import de.baeckerit.jface.util.ViewerComparatorWithGetter;
 import de.baeckerit.rcp.ui.getter.ViewCategoryImageGetter;
 import de.baeckerit.rcp.ui.getter.ViewCategoryLabelGetter;
 import de.baeckerit.rcp.ui.util.UI;
@@ -19,27 +19,26 @@ import de.baeckerit.rcp.ui.view.ViewPartWithViewer;
 
 public class ViewCategories extends ViewPartWithViewer<TableViewer> {
 
-    public void createPartControl(Composite parent) {
-        viewer = new TableViewer(parent);
-        viewer.setContentProvider(ArrayContentProvider.getInstance());
-        ViewCategoryLabelGetter labelGetter = ViewCategoryLabelGetter.INSTANCE;
-        ViewCategoryImageGetter imageGetter = ViewCategoryImageGetter.INSTANCE;
-        viewer.setLabelProvider(new TableLabelProviderWithGetters<>(labelGetter, imageGetter));
-        viewer.setSorter(new ViewerSorterWithGetter<>(labelGetter));
-        viewer.setInput(UI.getViewRegistry().getCategories());
+  public void createPartControl(Composite parent) {
+    viewer = new TableViewer(parent);
+    viewer.setContentProvider(ArrayContentProvider.getInstance());
+    ViewCategoryLabelGetter labelGetter = ViewCategoryLabelGetter.INSTANCE;
+    ViewCategoryImageGetter imageGetter = ViewCategoryImageGetter.INSTANCE;
+    viewer.setLabelProvider(new SingleColumnTableLabelProviderWithGetters<>(labelGetter, imageGetter));
+    viewer.setComparator(new ViewerComparatorWithGetter<>(labelGetter));
+    viewer.setInput(UI.getViewRegistry().getCategories());
 
-        viewer.addDoubleClickListener(new IDoubleClickListener() {
+    viewer.addDoubleClickListener(new IDoubleClickListener() {
 
-            @Override
-            public void doubleClick(DoubleClickEvent event) {
-                IViewCategory selected = JFACE.getFirstElement(IViewCategory.class, event);
-                try {
-                    UI.getActivePage().showView(ViewDescriptorsByCategory.ID, selected.getId(),
-                                    IWorkbenchPage.VIEW_ACTIVATE);
-                } catch (PartInitException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+      @Override
+      public void doubleClick(DoubleClickEvent event) {
+        IViewCategory selected = JFACE.getFirstElement(IViewCategory.class, event);
+        try {
+          UI.getActivePage().showView(ViewDescriptorsByCategory.ID, selected.getId(), IWorkbenchPage.VIEW_ACTIVATE);
+        } catch (PartInitException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 }
