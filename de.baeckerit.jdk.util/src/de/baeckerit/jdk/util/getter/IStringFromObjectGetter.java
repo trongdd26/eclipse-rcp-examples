be92d32318e388e90b989adb4a757243f0d391b2
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-08-26 08:57:48 +0000

    Changes for PM example

--- de.baeckerit.jdk.util/src/de/baeckerit/jdk/util/getter/IStringFromObjectGetter.java
+++
@@ -1,5 +0,0 @@
-package de.baeckerit.jdk.util.getter;
-
-public interface IStringFromObjectGetter extends IStringGetter<Object> {
-
-}
--- de.baeckerit.jdk.util/src/de/baeckerit/jdk/util/getter/IStringGetter.java
+++
@@ -1,6 +0,0 @@
-package de.baeckerit.jdk.util.getter;
-
-public interface IStringGetter<T> extends IGetter<String, T> {
-  @Override
-  public String get(T object);
-}
