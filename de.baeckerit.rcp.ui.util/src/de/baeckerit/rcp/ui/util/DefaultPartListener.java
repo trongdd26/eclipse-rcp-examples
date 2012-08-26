d4ac6502dc7010bd16abdb7f4b61ec6f0a30a2ac
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-08-26 08:44:45 +0000

    Changes for PM example

---
+++ de.baeckerit.rcp.ui.util/src/de/baeckerit/rcp/ui/util/DefaultPartListener.java
@@ -0,0 +1,77 @@
+package de.baeckerit.rcp.ui.util;
+
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IWorkbenchPartReference;
+
+/**
+ * Default implementation for IPartListener2.
+ * 
+ * @version $LastChangedRevision$
+ * @author Andreas Baecker
+ */
+public class DefaultPartListener implements IPartListener2 {
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partActivated(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partActivated(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partBroughtToTop(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partBroughtToTop(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partClosed(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partClosed(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partDeactivated(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partDeactivated(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partHidden(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partHidden(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partInputChanged(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partInputChanged(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partOpened(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partOpened(IWorkbenchPartReference partRef) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.IPartListener2#partVisible(org.eclipse.ui.IWorkbenchPartReference)
+     */
+    public void partVisible(IWorkbenchPartReference partRef) {
+    }
+}
