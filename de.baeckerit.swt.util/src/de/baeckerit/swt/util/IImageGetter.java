edaec831cbbbf63d7d1cfbe884f204ec2cb49491
rcp.entwicklung <rcp.entwicklung@gmail.com>
2012-08-26 08:43:00 +0000

    Changes for PM example

--- de.baeckerit.swt.util/src/de/baeckerit/swt/util/IImageGetter.java
+++
@@ -1,9 +0,0 @@
-package de.baeckerit.swt.util;
-
-import org.eclipse.swt.graphics.Image;
-
-import de.baeckerit.jdk.util.getter.IGetter;
-
-public interface IImageGetter<T> extends IGetter<Image, T> {
-  Image get(T object);
-}
