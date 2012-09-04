0f00f0e8219ec2831e773e573c4a61e7757fc454
rcp.entwicklung@gmail.com <rcp.entwicklung@gmail.com@a6d18dd1-c050-23d8-ddb3-4fc8853fe964>
2012-09-04 15:12:59 +0000

    Added validation for new securities (first/last trading days)

--- de.baeckerit.jface.databinding.util/src/de/baeckerit/jface/databinding/util/binding/TextFieldBindingFactory.java
+++ de.baeckerit.jface.databinding.util/src/de/baeckerit/jface/databinding/util/binding/TextFieldBindingFactory.java
@@ -1,6 +1,9 @@
 package de.baeckerit.jface.databinding.util.binding;
 
+import org.eclipse.core.databinding.validation.IValidator;
 import org.eclipse.osgi.util.NLS;
+
+import de.baeckerit.jface.databinding.util.validator.LengthValidator;
 
 /**
  * A text field binding is a text value binding with a length constraint.
@@ -23,5 +26,10 @@ public class TextFieldBindingFactory extends TextValueBindingFactory {
 
   public String getInputTooLongMessage() {
     return NLS.bind(DEFAULT_MESSAGE, "{0}", fieldName);
+  }
+
+  @Override
+  public IValidator getTargetToModelAfterConvertValidator() {
+    return new LengthValidator(getMaxLength(), getInputTooLongMessage());
   }
 }
--- de.baeckerit.jface.databinding.util/src/de/baeckerit/jface/databinding/util/binding/TextValueBindingFactory.java
+++ de.baeckerit.jface.databinding.util/src/de/baeckerit/jface/databinding/util/binding/TextValueBindingFactory.java
@@ -3,28 +3,13 @@ package de.baeckerit.jface.databinding.util.binding;
 import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.conversion.IConverter;
-import org.eclipse.core.databinding.validation.IValidator;
 
 import de.baeckerit.jface.databinding.util.DataBindingUtils;
 import de.baeckerit.jface.databinding.util.IControlValues;
 import de.baeckerit.jface.databinding.util.converter.TrimConverter;
-import de.baeckerit.jface.databinding.util.validator.LengthValidator;
 
 public class TextValueBindingFactory extends DefaultValueBindingFactory {
   public static final TextValueBindingFactory FACTORY = new TextValueBindingFactory();
-
-  public int getMaxLength() {
-    return Integer.MAX_VALUE;
-  }
-
-  public String getInputTooLongMessage() {
-    return null;
-  }
-
-  @Override
-  public IValidator getTargetToModelAfterConvertValidator() {
-    return new LengthValidator(getMaxLength(), getInputTooLongMessage());
-  }
 
   @Override
   public IConverter getTargetToModelConverter() {
